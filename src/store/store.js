58fa9552c85aa2b00b49f3dee783a6e067894df4
AbdulSamad676 <abdulsamad31650@gmail.com>
2024-12-26 11:59:30 +0500

    fix: redux store and router issues fixed

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import authReducer from "./authSlice";
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    auth: authReducer, // Ensure 'auth' matches the slice name used in `Header`
+  },
 });
 
 export default store;
