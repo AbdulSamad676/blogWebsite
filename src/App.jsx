d98d3d2d9105fda9f499c4018e7413694720f897
AbdulSamad676 <abdulsamad31650@gmail.com>
2024-12-26 09:56:12 +0500

    add: tailwind css added and redux store completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,48 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState, useEffect } from "react";
 import "./App.css";
-import envConfig from "./envConfig";
-
+import authService from "./appwrite_services/auth";
+import { login, logout } from "./store/authSlice";
+import { useDispatch } from "react-redux";
+import { Header, Footer } from "./components/index";
+import { Outlet } from "react-router-dom";
 function App() {
-  console.log(envConfig.projectUrl);
+  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .catch((error) => {
+        console.log("Error in getting current User", error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
 
-  return (
-    <>
-      <h1>Blog website with appwrite</h1>
-    </>
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+          {/* <Outlet/> */}
+          Appwrite Project Here
+        </main>
+
+        <Footer />
+      </div>
+    </div>
+  ) : (
+    <div className=" flex justify-center items-center text-2xl">
+      {" "}
+      loading...
+    </div>
   );
 }
 
